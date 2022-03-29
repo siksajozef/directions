73fef5418bc7c1654f0e335b55c6746513a6d292
siksajozef <70346027+siksajozef@users.noreply.github.com>
2022-03-29 21:57:03 +0200

    add files

---
+++ scripts.js
@@ -0,0 +1,19 @@
+let color = "purple";
+let number = 10;
+let word = "cool";
+
+if (color = "purple"){
+    $(".red").css("background", "green");
+}
+
+$(".yellow").text("XXX");
+
+if(number>100){
+    console.log("whoah, that's a big number.");
+}else{
+    console.log("just a regular number, please.");
+}
+
+if (word="cool"){
+    $(".green").text("Power of DOM");
+}
